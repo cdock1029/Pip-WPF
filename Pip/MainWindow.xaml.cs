3307a7b61f6799f42f804f52d06b887beef4fb2d
Conor Dockry <conordockry@gmail.com>
2024-12-15 18:02:24 -0500

    Enhance search functionality and update UI
    Updated SearchViewModel with new properties and behavior:
    - Added _hasSearchResults and _searchResults properties.
    - Modified SearchResults.CollectionChanged handler.
    - Simplified SearchText setter.
    - Made Search method public.
    - Commented out ClearResults command.
    
    Updated MainWindow.xaml for improved search UI:
    - Replaced ComboBoxEdit with PopupBaseEdit.
    - Configured PopupBaseEdit for binding and popup display.
    - Added CompositeCommandBehavior to search button.
    
    Adjusted MainWindow.xaml.cs for new UI changes:
    - Added using directive for Pip.UI.ViewModel.
    - Replaced SearchComboBox event handler.
    - Updated event handler logic for HasSearchResults.
    - Removed SearchComboBox unsubscription in MainWindow_Closing.

--- Pip/MainWindow.xaml.cs
+++ Pip/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Windows;
 using System.Windows.Interop;
 using Pip.UI.Properties;
 using Pip.UI.View.Types;
+using Pip.UI.ViewModel;
 
 namespace Pip.UI;
 
@@ -22,17 +23,14 @@ public partial class MainWindow
 		SourceInitialized += MainWindow_SourceInitialized;
 		Closing += MainWindow_Closing;
 
-		SearchComboBox.IsKeyboardFocusWithinChanged += SearchComboBox_IsKeyboardFocusWithinChanged;
+		PopupBaseEdit.IsKeyboardFocusWithinChanged += PopupBaseEdit_IsKeyboardFocusedWithinChanged;
 	}
 
-	//https://supportcenter.devexpress.com/ticket/details/t414923/comboboxedit-open-popup-on-focus
-	private void SearchComboBox_IsKeyboardFocusWithinChanged(object sender, DependencyPropertyChangedEventArgs e)
+	private void PopupBaseEdit_IsKeyboardFocusedWithinChanged(object sender, DependencyPropertyChangedEventArgs e)
 	{
-		if (e.NewValue is true)
-		{
-			Debug.WriteLine("opening popup");
-			SearchComboBox.ShowPopup();
-		}
+		var newVal = (bool)e.NewValue;
+		var hasResults = ((MainViewModel)DataContext).SearchViewModel.HasSearchResults;
+		if (newVal && hasResults) PopupBaseEdit.ShowPopup();
 	}
 
 	private void MainWindow_SourceInitialized(object? sender, EventArgs e)
@@ -58,7 +56,6 @@ public partial class MainWindow
 
 	private void MainWindow_Closing(object? sender, EventArgs e)
 	{
-		SearchComboBox.IsKeyboardFocusWithinChanged -= SearchComboBox_IsKeyboardFocusWithinChanged;
 		try
 		{
 			// Persist window placement details to application settings
