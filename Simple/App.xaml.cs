14d0f7b5576cba693c15579db7e9fbf07f3b7217
Conor Dockry <conordockry@gmail.com>
2024-12-29 21:18:03 -0500

    messing around with simple form of WPF app with side nav

---
+++ Simple/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using DevExpress.Xpf.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Pip.DataAccess;
+
+namespace Simple;
+
+public partial class App
+{
+	//private readonly ServiceProvider _serviceProvider;
+
+
+	public App()
+	{
+		CompatibilitySettings.UseLightweightThemes = true;
+		//ThemedWindow.UseNativeWindow = true;
+
+		//ApplicationThemeHelper.ApplicationThemeName = Theme.Win10SystemColorsName;
+		ApplicationThemeHelper.ApplicationThemeName = LightweightTheme.Win10SystemColors.Name;
+		//ApplicationThemeHelper.ApplicationThemeName = Theme.Win11System.Name;
+		ServiceCollection serviceCollection = [];
+		//ConfigureServices(serviceCollection);
+		//_serviceProvider = serviceCollection.BuildServiceProvider();
+
+		//Current.ThemeMode = ThemeMode.System;
+	}
+
+
+	protected override void OnStartup(StartupEventArgs e)
+	{
+		using var context = new PipDbContext(new DbContextOptions<PipDbContext>());
+		context.Database.Migrate();
+
+		//var mainWindow = new MainWindow { ThemeMode = ThemeMode.System };
+		var mainWindow = new MainWindow();
+		mainWindow.Show();
+	}
+
+	private static void ConfigureServices(ServiceCollection serviceCollection)
+	{
+		serviceCollection.AddDbContext<PipDbContext>();
+	}
+}
