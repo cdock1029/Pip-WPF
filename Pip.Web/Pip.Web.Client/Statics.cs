512545174cb62adebd0fcd425947f216434715ab
Conor Dockry <conordockry@gmail.com>
2025-04-21 20:08:55 -0400

    fix TermSpanSort, use Treasury model in Auctions page

--- Pip.Web/Pip.Web.Client/Statics.cs
+++ Pip.Web/Pip.Web.Client/Statics.cs
@@ -1,4 +1,5 @@
-﻿namespace Pip.Web.Client;
+﻿
+namespace Pip.Web.Client;
 
 public static class Statics
 {
@@ -7,6 +8,10 @@ public static class Statics
     public const string GridFont =
         "font-family: Consolas,'Cascadia Mono',monospace; font-variant-numeric: tabular-nums oldstyle-nums; font-variant-caps: normal; text-transform: uppercase;";
 
-    public static readonly GridSort<TreasuryItemViewModel> TermSpanSort = GridSort<TreasuryItemViewModel>
-        .ByAscending(t => t.TermDaysSpan);
+    public static readonly GridSort<Treasury> TermSpanSort = GridSort<Treasury>
+        .ByAscending(t =>
+            t.IssueDate != null && t.MaturityDate != null
+                ? t.MaturityDate.Value.DayNumber - t.IssueDate.Value.DayNumber
+                : 0
+        );
 }
