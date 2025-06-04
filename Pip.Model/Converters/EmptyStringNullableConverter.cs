45dc0d4f0c3e1286f7b638cefd84272644f64f0a
Conor Dockry <conordockry@gmail.com>
2025-06-04 16:36:22 -0400

    deps

--- Pip.Model/Converters/EmptyStringNullableConverter.cs
+++ Pip.Model/Converters/EmptyStringNullableConverter.cs
@@ -5,15 +5,15 @@ namespace Pip.Model.Converters;
 
 public class EmptyStringNullableConverter<T> : JsonConverter<T?> where T : struct
 {
-	public override T? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-	{
-		if (reader.TokenType != JsonTokenType.String) return JsonSerializer.Deserialize<T>(ref reader, options);
-		var value = reader.GetString();
-		return string.IsNullOrEmpty(value) ? null : JsonSerializer.Deserialize<T>(ref reader, options);
-	}
+    public override T? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType != JsonTokenType.String) return JsonSerializer.Deserialize<T>(ref reader, options);
+        string? value = reader.GetString();
+        return string.IsNullOrEmpty(value) ? null : JsonSerializer.Deserialize<T>(ref reader, options);
+    }
 
-	public override void Write(Utf8JsonWriter writer, T? value, JsonSerializerOptions options)
-	{
-		JsonSerializer.Serialize(writer, value, options);
-	}
+    public override void Write(Utf8JsonWriter writer, T? value, JsonSerializerOptions options)
+    {
+        JsonSerializer.Serialize(writer, value, options);
+    }
 }
